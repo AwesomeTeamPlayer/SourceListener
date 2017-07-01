e99ebfccfdada5b7a6076015a215d7cdbe971e5a
szymonides <skrzynski.szymon@gmail.com>
2017-07-01 14:24:50 +0200

    Empty first integral test

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./vendor/bin/phpunit ./tests/units
+./vendor/bin/phpunit ./tests
---
+++ tests/integration/Controllers/RegisterClientTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tests\integration\Controllers;
+
+class RegisterClientTest extends \PHPUnit_Framework_TestCase
+{
+	public function test()
+	{
+
+	}
+}
